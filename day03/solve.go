f063dd5920723e05777d0630be862477c3c770f4
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2025-12-11 01:39:03 -0500

    feat: completed day3

--- day03/solve.go
+++ day03/solve.go
@@ -8,35 +8,83 @@ import (
 	"github.com/smiggiddy/2025-aoc/aoctools"
 )
 
-func main() {
-	// data := aoctools.LoadFile("./day03/sample.txt")
-	data := aoctools.LoadFile("./day03/input.txt")
+func getLargetNumber(r []rune) int {
+	maxFirstDigit := 0
+	bestCombo := 0
+
+	for i := range r {
+		combo := maxFirstDigit*10 + int(r[i]) - 48
+		bestCombo = max(bestCombo, combo)
+		maxFirstDigit = max(maxFirstDigit, int(r[i])-48)
+	}
+	return bestCombo
+}
+
+func getLargetNumberGreedy(r []rune, start int, end int) (int, int) {
+	var largest int = 0
+	var nextIndex int = 0
+
+	for i := start; i < len(r)-end; i++ {
+		if ch := int(r[i]) - 48; largest < ch {
+			largest = ch
+			nextIndex = i + 1
+		}
+
+	}
+	return largest, nextIndex
+}
+
+func partOne(data string) {
 	str := strings.SplitSeq(data, "\n")
 	sum := 0
-	var num int
 
 	for s := range str {
-		var largest int = 0
-
-		for i := 0; i <= len(s)-2; i++ {
-			for j := 1; j <= len(s)-1; j++ {
-				if j > i {
-					tempStr := string(s[i]) + string(s[j])
-					num, _ = strconv.Atoi(tempStr)
-
-					if num > largest {
-						largest = num
-					}
-
-				}
-
-			}
-
+		if len(s) == 0 {
+			continue
 		}
+
+		runes := []rune(s)
+		largest := getLargetNumber(runes)
+
 		sum += largest
 
 	}
 
-	fmt.Println(sum)
+	fmt.Println("part one", sum)
 
+}
+
+func partTwo(data string) {
+	str := strings.SplitSeq(data, "\n")
+	sum := 0
+
+	for s := range str {
+		if len(s) == 0 {
+			continue
+		}
+
+		largest := ""
+		nextI := 0
+		num := 0
+		runes := []rune(s)
+
+		for i := 11; i >= 0; i-- {
+			num, nextI = getLargetNumberGreedy(runes, nextI, i)
+			largest += strconv.Itoa(num)
+
+		}
+		num, _ = strconv.Atoi(largest)
+		sum += num
+
+	}
+
+	fmt.Println("part two", sum)
+
+}
+
+func main() {
+	// data := aoctools.LoadFile("./day03/sample.txt")
+	data := aoctools.LoadFile("./day03/input.txt")
+	partOne(data)
+	partTwo(data)
 }
