e805abc108ae8ed0bd10ce06e68826f1f357d6e4
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2025-12-12 21:47:24 -0500

    feat: day05

--- day05/solve.go
+++ day05/solve.go
@@ -2,30 +2,64 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/smiggiddy/2025-aoc/aoctools"
 )
 
-func main() {
-	data := aoctools.LoadFile("./day05/sample.txt")
-	seq := strings.Split(data, "\n\n")
-	var groups, numRange []string
+type ranges struct {
+	start int
+	end   int
+}
 
-	// var found bool
-	for _, c := range seq {
-		fmt.Println(c)
-		// if i == "" {
-		// 	found = true
-		// 	continue
-		// }
-		// if !found {
-		// 	groups = append(groups, i)
-		// } else {
-		// 	numRange = append(numRange, i)
-		// }
+func sortGroup(s string) []ranges {
+	allRanges := []ranges{}
+	groups := strings.SplitSeq(s, "\n")
+
+	for group := range groups {
+		g := strings.Split(group, "-")
+		start, _ := strconv.Atoi(g[0])
+		end, _ := strconv.Atoi(g[1])
+		r := ranges{
+			start: start,
+			end:   end,
+		}
+		allRanges = append(allRanges, r)
 	}
 
-	fmt.Println(groups)
-	fmt.Println(numRange)
+	return allRanges
+
+}
+
+func partOne(data string) {
+	data = strings.TrimSpace(data)
+	seq := strings.Split(data, "\n\n")
+	g := seq[0]
+	n := seq[1]
+	groups := sortGroup(g)
+	numbers := strings.Split(n, "\n")
+	slices.Sort(numbers)
+	var sum int = 0
+
+	for _, s := range numbers {
+		target, _ := strconv.Atoi(s)
+		for _, group := range groups {
+			if group.start <= target && target <= group.end {
+				sum += 1
+				break
+			}
+		}
+	}
+
+	fmt.Println(sum)
+
+}
+
+func main() {
+	data := aoctools.LoadFile("./day05/input.txt")
+	// data := aoctools.LoadFile("./day05/sample.txt")
+	partOne(data)
+
 }
