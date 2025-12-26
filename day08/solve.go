17ab1a318a2410e10a9ad7c65735a59e61cacaa1
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2025-12-26 14:33:11 -0500

    feat: day8 progress

--- day08/solve.go
+++ day08/solve.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/smiggiddy/2025-aoc/aoctools"
 )
 
+type battery struct {
+	x, y, z int
+}
+
 func main() {
 	data := aoctools.LoadFile("./day08/sample.txt")
+	lines := strings.SplitSeq(data, "\n")
 
-	fmt.Println(data)
+	var batteries []battery
+
+	for l := range lines {
+		str := strings.TrimSpace(l)
+		s := strings.Split(str, ",")
+		x, _ := strconv.Atoi(s[0])
+		y, _ := strconv.Atoi(s[1])
+		z, _ := strconv.Atoi(s[2])
+		batt := battery{
+			x: x,
+			y: y,
+			z: z,
+		}
+		batteries = append(batteries, batt)
+	}
+
+	fmt.Println(batteries)
+
 }
